adc5dc520561feef0ece3821fd6bcf9d1cd61753
Prantik Ghosh <118633815+prantikg96@users.noreply.github.com>
2024-03-05 18:36:51 +0530

    To declare variables with naming conventions(camel-case)

--- js/4.variable.js
+++ js/4.variable.js
@@ -1,4 +1,4 @@
-// To declare variables:
+// To declare variables with naming conventions(camel-case: if using more than one word for naming variables, then use it as 'eggPrice'. Most recommended for JS):
 
 // 40
 var eggPrice = 40;
