671ad9d2759d2087484e16323eca09af9bef36d4
Prantik Ghosh <118633815+prantikg96@users.noreply.github.com>
2024-03-05 18:06:57 +0530

    maths knowledge req. for JS or any programming language

---
+++ js/mathsRequiredForJavaScript.js
@@ -0,0 +1,11 @@
+/*
+    1. Number: 10, 5, 20, 45, 69 etc.
+    2. Integer: 10, 5, 20, 45, 69 etc.
+    3. Float: 1.23, 5.78, 4.5, 21.87 etc.
+    4. Negative nos.: -5, -2, -45, -10, -71 etc.
+    5. Operations: Addition +, Substraction -, Division /, Multiplication *
+    6. Percentage %.
+    7. Average: (10+50+6+68+90) / 5
+    8. Remainder: Modulus or Mod or Modulo  %
+    9. Comparison: 10, 45, 5, 7, 2, 82, 1   Max=82  Min=1  Signs: >, <, ==, ===(Hard check, to shoe whether eqaul or not), != .
+*/
