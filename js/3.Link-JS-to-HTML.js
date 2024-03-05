429f8e47b6e0208abeecbca2368193b0219d67e5
Prantik Ghosh <118633815+prantikg96@users.noreply.github.com>
2024-03-05 17:46:55 +0530

    learn JS: Inline, Internal, External JS

---
+++ js/3.Link-JS-to-HTML.js
@@ -0,0 +1,14 @@
+// To show output in browser's console (most recommended):
+console.log(60+50);
+console.log('Hello World');
+
+// To show output in webpage:
+// document.write(50+50);
+// document.write("Hello world")
+
+// To show output as alert when opening page or reloading page (not much recommended to show output. mainly used in page submissions):
+// alert('Hello Everyone')
+// alert(40+40)
+
+// To show output in terminal with the help of node js (only console.log() will work. alert() & document.write() wont work in terminal as these two are mainly based on browsers):
+console.log(30+25)
