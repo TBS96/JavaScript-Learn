2fd21e204c0267d3d372877fcb9a22ac99da7aa0
prantikg96 <prantik.g1996@gmail.com>
2024-03-10 17:50:47 +0530

    conversion with parseInt(), parseFloat(), toFixed(), toPrecision(), Number(), String()

---
+++ js/16.Conversion.js
@@ -0,0 +1,33 @@
+// let num1 = "44";
+// let num2 = 33;
+// num1 = parseInt(num1);  // parseInt() converts a string into an integer.
+// const result = num1 + num2;
+// console.log(result);    // o/p: 77
+
+
+// let a = '55';
+let a = 'bgfrt';
+let result = Number(a);
+console.log(result);    // o/p: NaN (NaN stands for Not a Number)
+
+
+// let score1 = "7.9874";
+// let score2 = '10.21457';
+// score1 = parseFloat(score1);  // parseFloat() converts a string into a floating point number.
+// score2 = parseFloat(score2);
+// const totalScore = score1 + score2;
+// console.log(totalScore);
+
+
+// let n1 = 5.8888888;
+// n1 = n1.toFixed(2);      // o/p: 5.89
+// n1 = n1.toPrecision(4);     // o/p: 5.889
+// console.log(n1);
+
+
+// let x = 22;
+// console.log(x.toString());
+
+let b = '46';
+let res = String(b);
+console.log(res);
