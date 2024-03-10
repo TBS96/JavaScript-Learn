9da649bcf0182f3216a3c75f516239c4afaf45db
prantikg96 <prantik.g1996@gmail.com>
2024-03-10 16:40:01 +0530

    string concatenation

---
+++ js/15.StringConcatenation.js
@@ -0,0 +1,24 @@
+// let firstName = "Prantik";
+// let lastName = "Ghosh";
+// let age = 28;
+// const fullName = firstName + ' ' + lastName + ' ' + age;
+// console.log(fullName);      /// o/p: PrantikGhosh
+
+// Another way to do the above:
+// console.log(firstName + ' ' + lastName + ' ' + age);
+
+
+// let num1 = 10;
+// let num2 = 20;
+// const result = num1 + num2;
+// console.log(result);    // o/p:30
+
+// let n1 = 10;
+// n1 = 20;
+// let n2 = 20;
+// console.log(n1);
+// console.log(n1 + n2);   // o/p:30
+
+let n1 = "10";
+let n2 = 20;
+console.log(n1 + n2);   // o/p: 1020
