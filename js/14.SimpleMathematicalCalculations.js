d4ca47ae7447c6d861d4e2b2dbd5d927bc9a2107
prantikg96 <prantik.g1996@gmail.com>
2024-03-10 16:16:23 +0530

    simple mathematical operartions

---
+++ js/14.SimpleMathematicalCalculations.js
@@ -0,0 +1,12 @@
+// const eggPrice = 60;
+// const saltPrice = 40;
+// const totalPrice = eggPrice + saltPrice;
+// console.log(totalPrice);
+
+const teaPrice = 10;
+const biscuitPrice = 15;
+const cakePrice = 10;
+const totalPrice = teaPrice + biscuitPrice + cakePrice;
+console.log("Total Price =", teaPrice + biscuitPrice + cakePrice);
+
+// similarly we can perform operations like +, - , *, /, %
