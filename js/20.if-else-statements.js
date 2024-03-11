e1e742f8121bc059773f4549442de82e56ac75d1
prantikg96 <prantik.g1996@gmail.com>
2024-03-11 18:46:20 +0530

    if, else if, else statements

---
+++ js/20.if-else-statements.js
@@ -0,0 +1,23 @@
+let score = 80;
+
+if(score >= 90){
+    console.log("A+");
+}
+else if(score >= 80){   //printed
+    console.log("A");
+}
+else if(score >= 70){
+    console.log("B+");
+}
+else if(score >= 60){
+    console.log("B");
+}
+else if(score >= 50){
+    console.log("C");
+}
+else if(score >= 40){
+    console.log("D");
+}
+else{
+    console.log("Fail");
+}
