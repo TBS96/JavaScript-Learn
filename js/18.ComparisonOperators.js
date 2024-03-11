85adb40174efba762d01c9f34d35901292393034
prantikg96 <prantik.g1996@gmail.com>
2024-03-11 17:15:43 +0530

    comparison operators

---
+++ js/18.ComparisonOperators.js
@@ -0,0 +1,12 @@
+console.log(4 < 9);     //true
+console.log(4 > 9);     //false
+console.log(4 == 4);     //true
+console.log(4 == '4');     //true
+console.log(4 === '4');     //false     checks with datatype
+console.log(5 != 4);     //true
+console.log(5 != '5');     //false
+console.log(5 !== '5');     //true
+console.log(5 <= 6);     // true
+console.log(5 >= 6);     // false
+console.log(5 >= 5);     // true
+console.log(5 <= 5);     // true
