dc77d433bde332872e48a621ceaf4b4132933a1f
prantikg96 <prantik.g1996@gmail.com>
2024-03-11 17:37:52 +0530

    conditonal statements with if-else-1

---
+++ js/19.conditionalStatements.js
@@ -0,0 +1,27 @@
+let burgerPrice = 90;
+let myBudget = 70;
+
+// if burgerPrice is less than myBudget, then i'll have it:
+// if(burgerPrice < myBudget){
+    // console.log("I'll have it");    //condition is false, so no o/p
+// }
+
+// if burgerPrice is greater than myBudget, then cant have it:
+// if(burgerPrice > myBudget){
+    // console.log("I cant have it");    //condition is false
+// }
+
+// Now updating the price of myBudget to 70 & check if burgerPrice is less than or equal to myBudget, then i'll have it:
+// myBudget = 90;
+// if(burgerPrice <= myBudget){
+//     console.log("I'll have it");    //condition is true
+// }
+
+// Now updating the price of myBudget to 100 & check if burgerPrice is greater than or equal to myBudget, then i cant have it:
+myBudget = 100;
+if(burgerPrice >= myBudget){
+    console.log("I cant have it");    //condition is false
+}
+else{
+    console.log("I can have it");   // printed
+}
