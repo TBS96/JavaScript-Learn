7051635123ffdd21711d51490bd215f1f778cfb4
prantikg96 <prantik.g1996@gmail.com>
2024-03-11 14:52:45 +0530

    string concatenation: minor changes

--- js/15.StringConcatenation.js
+++ js/15.StringConcatenation.js
@@ -1,24 +1,29 @@
 // let firstName = "Prantik";
 // let lastName = "Ghosh";
 // let age = 28;
-// const fullName = firstName + ' ' + lastName + ' ' + age;
-// console.log(fullName);      /// o/p: PrantikGhosh
+// const fullNameAge = firstName + ' ' + lastName + ' ' + age;
+// console.log(fullNameAge);      /// o/p: Prantik Ghosh 28
 
 // Another way to do the above:
 // console.log(firstName + ' ' + lastName + ' ' + age);
 
 
+// Using integers:
 // let num1 = 10;
 // let num2 = 20;
 // const result = num1 + num2;
 // console.log(result);    // o/p:30
 
+
+// Updating n1:
 // let n1 = 10;
 // n1 = 20;
 // let n2 = 20;
-// console.log(n1);
-// console.log(n1 + n2);   // o/p:30
+// console.log(n1);  // o/p: 20
+// console.log(n1 + n2);   // o/p:40
 
+
+// Incase, user declares number in string format:
 let n1 = "10";
 let n2 = 20;
 console.log(n1 + n2);   // o/p: 1020
