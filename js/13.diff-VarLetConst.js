295537318ec99e59b24e5be6d0b1023bffe50228
prantikg96 <prantik.g1996@gmail.com>
2024-03-11 12:45:33 +0530

    var, let, const difference: rectifying some typos

--- js/13.diff-VarLetConst.js
+++ js/13.diff-VarLetConst.js
@@ -1,6 +1,7 @@
-// var name = "Prantik";
-// var name = "Deepa";
-// var name = "Subhash";
+// var names = "Prantik";
+// var names = "Deepa";
+// var names = "Subhash";
+// console.log(names);
 
 // var keyword creates problems while declaring variables of same names. to minimize this, JS v.ES6 brought 'let' and 'const' keywords, to declare variables.
 
@@ -11,7 +12,7 @@
 // console.table([x, y, z]);
 
 
-// If we use 'let' for declaring a variable with same name, then compiler will show a syntax error mentioning "Identifier 'x' has already been declared". So unlike var keyword, 'let' helps us from completely minimizing these errors in code.
+// If we use 'let' for declaring a variable with same name (if more than one), then compiler will show a syntax error mentioning "Identifier 'x' has already been declared". So unlike var keyword, 'let' helps us from completely minimizing these errors in code.
 // let x=10;
 // let x=50;
 // console.log(x);
@@ -22,7 +23,7 @@
 // console.log(num);   // o/p: 50
 
 
-// If we use 'const' for declaring a variable with same name, then compiler will show a syntax error mentioning "Identifier 'num' has already been declared". So unlike var keyword, 'const' helps us from completely minimizing these errors in code.
+// Similarly, if we use 'const' for declaring a variable with same name (if more than one), then compiler will show a syntax error mentioning "Identifier 'num' has already been declared". So unlike var keyword, 'const' helps us from completely minimizing these errors in code.
 // const num = 20;
 // const num = 50;
 // console.log(num);
@@ -33,4 +34,4 @@ num = 80;
 console.log(num);   // We get a TypeError as 'const' means constant, so it shows an error "Assignment to constant variable." This means we cant update a variable's value when using 'const'.
 
 // Note:
-// 'let' helps us to update or replace or reassign a variable's value with same name, whereas in 'const', it wont allow us to update to replace.
+// 'let' helps us to update or replace or reassign a variable's value with same name, whereas in 'const', it wont allow us to update or replace.
