9e4b2266b0a62996c7fa457997340944ff8b228c
prantikg96 <prantik.g1996@gmail.com>
2024-03-11 16:50:25 +0530

    operations using shorthand techniques

---
+++ js/17.shortHand.js
@@ -0,0 +1,19 @@
+// let price1 = 30;
+// let price2 = 20;
+// console.log(price1 + price2);
+
+
+// shortcut or shorthand technique:
+// let price1 = 30;
+// price1 += 20;
+// price1 -= 20;
+// price1 *= 20;
+// price1 /= 20;
+// price1 %= 20;
+// console.log(price1);
+
+// to add 1 to any variable using shorthand:
+let price = 50;
+price++;    //+1
+price--;    //-1
+console.log(price);     // o/p: 51
