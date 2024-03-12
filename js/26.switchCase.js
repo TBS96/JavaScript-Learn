17651a328c404e72d9aa67dc9b35dbe5cdcffd08
prantikg96 <prantik.g1996@gmail.com>
2024-03-12 16:52:13 +0530

    switch case

---
+++ js/26.switchCase.js
@@ -0,0 +1,15 @@
+let x=2;
+switch(x)
+{
+    case 0:
+        console.log("Zero");
+        break;
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    default:
+        console.log("Error");
+}
