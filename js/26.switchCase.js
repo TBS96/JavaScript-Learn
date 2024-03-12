fa467aa7c21a9a9cac5c52c9cc2997fb4910996d
prantikg96 <prantik.g1996@gmail.com>
2024-03-12 22:21:00 +0530

    input as prompt & display in browser

--- js/26.switchCase.js
+++ js/26.switchCase.js
@@ -13,3 +13,28 @@ switch(x)
     default:
         console.log("Error");
 }
+
+
+// Taking as user input in Node.js:
+// const readline = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// readline.question('Enter value of x: ', (x) => {
+//     x = parseInt(x);
+//     switch(x) {
+//         case 0:
+//             console.log("Zero");
+//             break;
+//         case 1:
+//             console.log("One");
+//             break;
+//         case 2:
+//             console.log("Two");
+//             break;
+//         default:
+//             console.log("Error");
+//     }
+//     readline.close();
+// });
