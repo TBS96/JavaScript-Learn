55300f4c391c24c01e2e3ea363739db7469cd61e
prantikg96 <prantik.g1996@gmail.com>
2024-03-12 14:28:50 +0530

    added example-2 with while()

--- js/23.whileLoop.js
+++ js/23.whileLoop.js
@@ -1,6 +1,16 @@
+// Example - 1:
 let count = 1;
 while(count <= 5)
 {
     console.log("Hello World", count);
     count++;
+}
+
+// Example - 2:
+const arrayName = ['Prantik', 'Deepa', 'Subhash', 'Rahim'];
+let c = 0;
+while(c < arrayName.length)
+{
+    console.log(arrayName[c]);
+    c += 2;
 }
