4c76117ec2f961f4cffa286e51422ed864b707d7
prantikg96 <prantik.g1996@gmail.com>
2024-03-12 23:03:09 +0530

    indexOf a in Subhash

--- js/5.array-indexOf-arrayLength-methods.js
+++ js/5.array-indexOf-arrayLength-methods.js
@@ -1,6 +1,6 @@
 var publicsAmt = [20, 14, 69, 78, 100, 14, 56, 50, 1000];
 var friendsName = ["Prantik", "Deepa", "Subhash"];
-
+var s="Subhash";
 var indexOfPublicsAmt = publicsAmt[5];
 var indexOfFriendsName = friendsName[1];
 // console.log(typeof publicsAmt);     // Output: object
@@ -14,7 +14,7 @@ console.log("Length of friendsName array = ", friendsName.length);
 // To find position of array elements:
 console.log("IndexOf publicsAmt = ", publicsAmt.indexOf(1000));
 console.log("IndexOf friendsName = ", friendsName.indexOf("Subhash"));
-
+console.log("IndexOf a in Subhash = ",s.indexOf("a"));
 
 // To find the element of declared index:
 console.log(indexOfPublicsAmt);
