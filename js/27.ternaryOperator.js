de258bdefaada66d4bd5bba5eb7feb4d40c80009
prantikg96 <prantik.g1996@gmail.com>
2024-03-12 17:18:00 +0530

    ternary operator & nested ternary operator

---
+++ js/27.ternaryOperator.js
@@ -0,0 +1,20 @@
+// let age=18, vote;
+// if(age>=18)
+// {
+//     vote="Eligible";
+// }
+// else
+// {
+//     vote="Not eligible";
+// }
+// console.log(vote);
+
+// to write the above in short, we use ternary operator:
+// let age=17;
+// let vote=(age>=18)? "Eligible" : "Not eligible";
+// console.log(vote);
+
+// Nested ternary:
+let age=12;
+let vote=(age>=18) ? "Eligible" : (age<10) ? "Bari jao" : "Young";
+console.log(vote);
