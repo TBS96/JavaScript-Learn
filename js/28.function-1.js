3a473f3de56e7df3af07438302690d1faa69e436
prantikg96 <prantik.g1996@gmail.com>
2024-03-13 16:11:13 +0530

    function-1: creating(defining function without parameter & and with parameter + arguments

---
+++ js/28.function-1.js
@@ -0,0 +1,22 @@
+// creating(defining) function without parameter:
+// function myFunc1()
+// {
+//     console.log("My name is Prantik & I'm from India.");
+//     console.log("My name is Tom Cruise & I'm from USA.");
+//     console.log("My name is Jackie Chan & I'm from Hong Kong.");
+// }
+
+// calling function:
+// myFunc1();
+
+
+// creating(defining) function with parameter:
+function myFunc2(name, country)
+{
+    console.log("My name is " + name + " & I'm from " + country);
+}
+
+// calling function with arguments:
+myFunc2("Prantik","India");
+myFunc2("Jackie Chan","Hong Kong");
+myFunc2("Tom Cruise","USA");
