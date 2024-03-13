d7f7876f0a1e2196ad6951575e793e5cd1560d81
prantikg96 <prantik.g1996@gmail.com>
2024-03-13 16:51:36 +0530

    defining & calling function with return value

---
+++ js/29.function-2.js
@@ -0,0 +1,17 @@
+// defining function with parameter:
+function addNum(a, b)
+{
+    // console.log(`Sum of ${a} and ${b} =`, a+b);
+    // console.log("Sum of " + a + " and " + b + " =", a+b);
+
+    // to take this outsde the function using 'return' value:
+    let total= a+b;
+    return total;
+}
+
+// calling function with arguments:
+// addNum(40,60);
+
+// calling function outside with arguments from return value:
+const result = addNum(40,90);
+console.log(result);
